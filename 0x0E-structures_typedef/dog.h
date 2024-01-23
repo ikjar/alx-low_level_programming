a57d59e70e32c51bb3bce43dfe77bb3f8d977669
ikjar <hajarikke@gmail.com>
2024-01-23 15:26:48 -0800

    0.poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - data type for a dog characteristics.
+ * @name : character describe dog's name.
+ * @age: Integer describe dog's age.
+ * @owner: character describe dog's owner.
+ * Description: the dogs are the best friends.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
